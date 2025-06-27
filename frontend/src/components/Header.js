6dae106e4b36a1673b9c732c45eb912e1465d461
bazir21 <bayir@live.ie>
2025-06-27 03:38:36 +0100

    update header styling

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -3,7 +3,9 @@ import "./Header.css";
 function Header() {
   return (
     <header className="Header">
+      <img className="logo" src="nasa_logo.png" alt="NASA Logo"/>
       <h1>Nasa Data Explorer</h1>
+      <div />
     </header>
   );
 }
