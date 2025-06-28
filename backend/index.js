b471beeed4a0c4222acabdb5196e396a92732613
bazir21 <bayir@live.ie>
2025-06-28 22:39:23 +0100

    add items for heroku deployment

--- backend/index.js
+++ backend/index.js
@@ -119,6 +119,12 @@ app.get("/TLE", async (req, res) => {
   }
 });
 
+app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
